a8ae41cf2f7608e67312e80f3608bdfc919cfa39
kazawa03 <131092055+kazawa03@users.noreply.github.com>
2024-01-05 01:28:29 +0900

    Update ex1-2.js

--- ex1-2.js
+++ ex1-2.js
@@ -1,5 +1,2 @@
-function imgchange1(){document.getElementById('akabeko').src='./image/akabeko.png';}
-function imgchange2(){document.getElementById('akabeko').src='./image/hukushimaken.png';}
-
-function imgchange1(){document.getElementById('logo').src='./image/akabeko.png';}
-function imgchange2(){document.getElementById('logo').src='./image/'hukushimaken.png;}
+function imgchange1(){document.getElementById('logo').src='./image/'akabeko.png';}
+function imgchange2(){document.getElementById('logo').src='./image/'hukushimaken.png';}
