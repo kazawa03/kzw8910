be97eea46572c6845ad51ae2e00662db5d37d9c9
kazawa03 <131092055+kazawa03@users.noreply.github.com>
2024-01-05 01:12:20 +0900

    Update ex1-2.js

--- ex1-2.js
+++ ex1-2.js
@@ -1,2 +1,2 @@
-function imgchange1(){document.getElementById('akabeko').src='./image/akabeko.png';}
-function imgchange2(){document.getElementById('akabeko').src='./image/hukushima.png';}
+function imgchange1(){document.getElementById('akabeko').src='./image/hukushima.png';}
+function imgchange2(){document.getElementById('akabeko').src='./image/akabeko.png';}
