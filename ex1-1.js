9ff2c77a8cdff91c848075788ddf6540e94af83c
kazawa03 <131092055+kazawa03@users.noreply.github.com>
2024-01-05 00:12:55 +0900

    Update ex1-1.js

--- ex1-1.js
+++ ex1-1.js
@@ -1,1 +1,4 @@
-
+function hyoji(){ var str = "賀澤佑希南です。"; var cnt = document.timer.moji.value.length; 
+if ( cnt < 11 ) { document.timer.moji.value = str.substr(0,cnt+1); }
+else { document.timer.moji.value = ""; } }   
+function startfnc() { setInterval("hyoji()",1000); } 
